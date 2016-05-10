98ce7a3b04ac98527f243e3feae8c5a9a20e290b
Rebecca Turner <637275@gmail.com>
2016-05-10 12:43:38 -0400

    partial revamp of measure-unit with more options. not working at all, dont check out

--- size.psm1
+++ size.psm1
@@ -1,67 +1,154 @@
 function Measure-Unit {
-	If( $args[0] -eq "-help" -or
-		$args[0] -eq "--help" -or
-		$args[0] -eq "-?" -or
-		$args[0] -eq $null )
-	{
-		"Takes in a list of numbers (of bytes) and returns an array"
-		"of the appropriate unit for each value"
-		"Note that the unit text for `"bytes`" is `"`", as bytes are"
-		"usually written plain."
-		"If only one number is passed in, only the object will be"
-		"returned (not in an array)"
-		"`n`rExample usage:"
-		"PS>Measure-Unit 100 1500 1500000"
-		"[0]: Text: "
-		"     Unit: 1"
-		"[1]: Text: kb"
-		"     Unit: 1024"
-		"[2]: Text: mb"
-		"     Unit: 1048576"
-		return
-	}
+<#
+.SYNOPSIS
+	Gives the appropriate unit for any byte amount
+
+.DESCRIPTION
+	Takes in a list of numbers (of bytes) and returns an array of the appropriate unit for each value, with each element having a Text and Unit key. Note that the default unit text for "bytes" is "", as bytes are usually written plain.  If only one number is passed in, only the object will be returned (not in an array)
+
+	Example usage:
+	PS>Measure-Unit 100 1500 1500000
+	Name                           Value
+	----                           -----
+	Text
+	Unit                           1
+	Text                           kb
+	Unit                           1024
+	Text                           kb
+	Unit                           1024
+
+.PARAMETER Units
+	An array of int values to be processed. Implicit in unnamed arguments if not specified.
+
+.PARAMETER Lower
+	If Measure-Unit is called with -Lower, the lower unit will be used if there would be only 1 of the higher unit. E.g. 1048576 = 1mb would return kb rather than mb.
+
+.PARAMETER BytesText
+	By default, values >1024 will have no unit text. However, BytesText can specify an optional text.
+
+.PARAMETER Uppercase
+	Aliased as "Caps" and "Capital". If Measure-Unit is called with -Uppercase, text will be in uppercase. E.g. "MB" instead of "mb"
+
+.PARAMETER Long
+	
+
+#>
+
+	[CmdletBinding()]
+	Param(
+			#get unnamed params for units
+			[Parameter(
+				 Position = 0
+				,ValueFromRemainingArguments = $True
+				)
+			]
+			[Int[]]$Units,
+
+			[Switch]$Lower = $False,
+
+			[String]$BytesText = "",
+
+			[Alias("Capital", "Caps")]
+			[Switch]$Uppercase = $False,
+
+			[Switch]$Long = $False
+		 )
+
 	$OutItems = New-Object System.Collections.ArrayList
-	ForEach( $Size in $args )
+	ForEach( $Size in $Units )
 	{
+		#dirty hack
+		#if the value is lower by 1, lower unit is returned
+		#switch to bool to int, $true = 1, $false = 0
+		#definitely cleaner than an if() but possibly less... good
+		$Size -= [Int][Bool]$Lower
+
+		#test units
 		If( $Size -lt 1kb )
 		{
 			$Unit = 1
-			$UnitText = ""
+			$UnitText = $BytesText
 		}
 		ElseIf( $Size -lt 1mb )
 		{
 			$Unit = 1kb
-			$UnitText = "kb"
+			$UnitText = "kilo"
 		}
 		ElseIf( $Size -lt 1gb )
 		{
 			$Unit = 1mb
-			$UnitText = "mb"
+			$UnitText = "mega"
 		}
 		ElseIf( $Size -lt 1tb )
 		{
 			$Unit = 1gb
-			$UnitText = "gb"
+			$UnitText = "giga"
 		}
 		ElseIf( $Size -lt 1pb )
-		{ #i hope nobody ever needs this
+		{
+			#if you ever ask powershell to measure the size of
+			#a terabytes-large file, godspeed. may you never
+			#need this case
 			$Unit = 1tb
-			$UnitText = "tb"
+			$UnitText = "tera"
 		}
-	$OutItems.Add(
+
+		If( $Long -and (-not ($Size -lt 1kb -and $BytesText) ))
+		{
+			#if we want longer units, add "bytes" unless
+			#called with -BytesText, in which case skip
+			$UnitText += "$(if( $BytesText )
+			{
+				$BytesText
+			}
+			else
+			{
+				'bytes'
+			})"
+		}
+		ElseIf( ! ($Long -and $Size -lt 1kb) )
+		{
+			$UnitText = "$($UnitText[0])b"
+		}
+
+		#if called with -Uppercase make it uppercase
+		if( $Uppercase )
+		{
+			$UnitText = $UnitText.ToUpper();
+		}
+
+		#add elem to out array, don't display output
+		$OutItems.Add(
 			@{Unit = $Unit
 			Text = $UnitText
-			}) > $null
+		}) > $null
 	}
+
 	return $OutItems
 }
 
 function Format-Unit
 {
-	If( $args[0] -eq "-help" -or
-		$args[0] -eq "--help" -or
-		$args[0] -eq "-?" -or
-		$args[0] -eq $null )
+	[CmdletBinding()]
+	Param(
+			[Switch]$help = $False,
+			
+			[Parameter(
+				Mandatory = $False
+				)
+			]
+			[Int]$Decimals = 2,
+
+			[Parameter(
+				 Position = 0
+				,ValueFromRemainingArguments = $True
+				)
+			]
+			[String[]]$Amounts
+
+		 )
+
+	If( $help -or ! $Amounts)
 	{
 		"Takes in a list of numbers (of bytes) and returns an array"
 		"of the appropriately formatted units for each value"
@@ -73,11 +160,10 @@ function Format-Unit
 		return
 	}
 	$OutItems = New-Object System.Collections.ArrayList
-	ForEach( $Size in $args )
+	ForEach( $Size in $Amounts )
 	{
 		$Units = Measure-Unit $Size
 
-		$Decimals = 2
 		If($Units.Text -eq "") {
 #Would be kinda weird to show 12.00 bytes
 			$Decimals = 0
@@ -97,6 +183,7 @@ function Get-Size
 			[Parameter(
 				 Position=0
 				,ValueFromPipeline=$True
+				,ValueFromRemainingArguments = $True
 				)
 			]
 			[String]$PathSpec = ".\",
