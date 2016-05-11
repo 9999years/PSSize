4c44899ad44765d10d397834788db02341f28529
Rebecca Turner <637275@gmail.com>
2016-05-10 20:28:02 -0400

    Format-Unit fixed, except for pipeline input. help docs havent been written either yet #TODO

--- size.psm1
+++ size.psm1
@@ -83,8 +83,8 @@ function Measure-Unit {
 			]
 			[Int[]]$Units,
 
-			[Alias("RoundDown")]
-			[Switch]$Lower = $False,
+			[Alias("Lower")]
+			[Switch]$RoundDown = $False,
 
 			[Switch]$BytesText = $False,
 
@@ -103,7 +103,7 @@ function Measure-Unit {
 
 		ForEach( $Size in [Array]$Units )
 		{
-			$Size -= [Int][Bool]$Lower
+			$Size -= [Int][Bool]$RoundDown
 			#dirty hack
 			#if the value is lower by 1, lower unit is returned
 			#cast to bool to int, $true = 1, $false = 0
@@ -173,40 +173,111 @@ function Measure-Unit {
 				Text = $UnitText
 			}) > $null
 		}
-		return $OutItems
+		return [Array]$OutItems
 	}
 }
 
 function Format-Unit
 {
+<#
+	If( $help -or ! $Amounts)
+	{
+		"Takes in a list of numbers (of bytes) and returns an array"
+		"of the appropriately formatted units for each value"
+		"`n`rExample usage:"
+		"PS>Format-Unit 100 1500 1500000"
+		"[0]: 100"
+		"[1]: 1.46kb"
+		"[2]: 1.43mb"
+		return
+	}
+.SYNOPSIS
+	Gives the appropriate unit for any byte amount
+
+.DESCRIPTION
+	Takes in a list of numbers (of bytes) and returns an array of the appropriate unit for each value, with each element having a Text (a string like "mb" or "kb") and Unit (an int like 1024 or 1048576) key.
+	Note that the default unit text for "bytes" is "", as bytes are usually written plain.
+	If only one number is passed in, only the object will be returned (not in an array).
+	Accepts pipeline input.
+
+.PARAMETER Units
+	An array of int values to be processed. Implicit in unnamed arguments if not specified.
+
+.PARAMETER RoundDown
+	Aliased as "Lower". If Measure-Unit is called with -RoundDown, the lower unit will be used if there would be only 1 of the higher unit. E.g. 1048576 = 1mb would return kb rather than mb.
+
+.PARAMETER BytesText
+	By default, values >1024 will have no unit text. However, if -BytesText is passed, the unit text for bytes will be "b" (or "bytes" if passed in tandem with -Long).
+
+.PARAMETER UpperCase
+	Aliased as "Caps" and "Capital". If Measure-Unit is called with -UpperCase, text will be in uppercase. E.g. "MB" instead of "mb".
+
+.PARAMETER TitleCase
+	Capitalizes the first letter of each unit. Overriden by -UpperCase if both are passed.
+
+.PARAMETER Long
+	Enables un-abbreviated output, E.g. "10 kilobytes" instead of "10 kb".
+
+.COMPONENT
+	Measure-Unit
+
+#>
 
 	[CmdletBinding()]
 	Param(
-			[Int]$Decimals = 2,
-
 			[Parameter(
 				Position = 0,
 				ValueFromRemainingArguments = $True,
-				AcceptFromPipeline = $True,
+				ValueFromPipeline = $True,
 				Mandatory = $True
 				)
 			]
-			[String[]]$Amounts
+			[String[]]$Amounts,
 
+			[Int]$Decimals = 2,
+
+			[Switch]$ExtraByteDigits = $False,
+
+			[Alias("Lower")]
+			[Switch]$RoundDown = $False,
+
+			[Switch]$BytesText = $False,
+
+			[Alias("Capital", "Caps")]
+			[Switch]$UpperCase = $False,
+
+			[Switch]$TitleCase = $False,
+
+			[Switch]$Long = $False,
+
+			[Switch]$NoSpace = $False
 		 )
 
 	$OutItems = New-Object System.Collections.ArrayList
-	ForEach( $Size in $Amounts )
-	{
-		$Units = Measure-Unit $Size
+	[Array] $Units = Invoke-Expression "Measure-Unit $Amounts $(
+		if($RoundDown) { "-RoundDown " }
+		if($BytesText) { "-BytesText " }
+		if($UpperCase) { "-UpperCase " }
+		if($TitleCase) { "-TitleCase " }
+		if($Long) { "-Long "}
+		)"
 
-		If($Units.Text -eq "") {
-#Would be kinda weird to show 12.00 bytes
-			$Decimals = 0
+	ForEach( $i in 0..($Amounts.Count - 1) )
+	{
+		If($Units[$i].Unit -eq 1 -and ! $ExtraByteDigits) {
+			#Would be kinda weird to show 12.00 bytes
+			$TempDecimals = 0
+		}
+		Else
+		{
+			$TempDecimals = $Decimals
 		}
 
 		$OutItems.Add(
-			"{0:N$($Decimals)} $($Units.Text)" -f ($Size / $Units.Unit)
+			"{0:N$($TempDecimals)}$(
+			if( ! $NoSpace ) { " " }
+			)$($Units[$i].Text)" -f
+			($Amounts[$i] / $Units[$i].Unit)
 			) > $null
 	}
 	return $OutItems
@@ -253,11 +324,11 @@ function Get-Size
 		return
 	}
 
-	$Items = iex "(Get-ChildItem $PathSpec -recurse $(if( $Force ){`"-Force`"}) | Measure-Object -property length -sum)"
+	$Items = Invoke-Expression "(Get-ChildItem $PathSpec -recurse $(if( $Force ){`"-Force`"}) | Measure-Object -property length -sum)"
 
 	Write-Verbose "The cumulative size of all $($Items.Count) file$(if($Items.Count -ne 1){'s'}) matching $($PathSpec)"
 
-	Write-Verbose (iex "([String](Get-ChildItem $PathSpec -recurse $(if( $Force ){`"-Force`"})))")
+	Write-Verbose (Invoke-Expression "([String](Get-ChildItem $PathSpec -recurse $(if( $Force ){`"-Force`"})))")
 	$Average = $Items.sum/$Items.count
 	Write-Verbose "(average of $(Format-Unit $Average) / file)"
 
